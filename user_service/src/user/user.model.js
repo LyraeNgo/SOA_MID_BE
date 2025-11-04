cd42bf93b541272a222fcb5141ac66695c13c383
Tamnm <ngominhtam26112005@gmail.com>
2025-11-04 23:55:06 +0700

    [20251104][Tamnm] remake user service

--- user_service/src/user/user.model.js
+++ user_service/src/user/user.model.js
@@ -1,20 +1,37 @@
+import { randomUUID } from "crypto";
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
-    username: { type: String, required: true, trim: true },
-    phoneNumber: { type: String, required: true },
-    email: { type: String, required: true, unique: true, lowercase: true },
-    studentId: { type: String, unique: true },
-    passwordHash: { type: String, required: true },
-    balance: { type: Number, required: true },
-    tuition: { type: Number, default: 0 },
-    transactions: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Transaction",
-      },
-    ],
+    userid: {
+      type: String,
+      default: () => randomUUID(), // UUID tự sinh khi tạo user mới
+      unique: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    passwordHash: {
+      type: String,
+      required: true,
+    },
+    balance: {
+      type: Number,
+      required: true,
+      default: 50000000,
+    },
   },
   { timestamps: true }
 );
